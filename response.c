45dd5f15e759128909491d0ef2afffcfa6aecb0c
GuangshanWen <wensjtuer@gmail.com>
2016-07-09 10:34:41 +0800

    update response.c

--- response.c
+++ response.c
@@ -1,3 +1,4 @@
+#include<unistd.h>
 #include<fcntl.h>
 #include<string.h>
 #include"response.h"
@@ -7,7 +8,7 @@
 
 static void parse_http_uri(char*,char*);
 static void io_write(char*,int);
-static void http_header(int);
+static void http_header(int,int);
 
 int http_response(struct wen_request* request)
 {
@@ -21,7 +22,7 @@ int http_response(struct wen_request* request)
 	return 1; 
 }
 
-static void parse_http_uri(char* url,char*fiename)
+static void parse_http_uri(char* url,char* filename)
 {
 	if(strcmp(url,"/") == 0)
 		filename = "index.html";
@@ -31,14 +32,20 @@ static void parse_http_uri(char* url,char*fiename)
 
 static void io_write(char* filename,int fd)
 {
-	http_header();
-
 	char path[100] = "html/";
 	strcat(path,filename);
-
-	int file = open(path,O_RDONLY);
-
 	char buf[1024];
+
+	int file = open((char*)path,O_RDONLY);
+
+	if(file == -1)
+	{
+		http_header(fd,-1);
+		int c = sprintf(buf,"cannot find this file in the server,please check again!\n");
+		write(fd,buf,c);
+		return ;
+	}
+	http_header(fd,1);
 	int count = read(file,buf,sizeof buf);
 	
 	while(count != 0)
@@ -46,11 +53,26 @@ static void io_write(char* filename,int fd)
 		write(fd,buf,count);
 		count = read(file,buf,sizeof buf);
 	}
+
+	close(file);
 }
 
-static void http_header(int fd)
+static void http_header(int fd,int flag)
 {
-	
+	int count = 0;
+	char buf[1024];
+	if(flag == 1)count = sprintf(buf,"HTTP/1.1 200 OK\r\n");
+	else if(flag == -1)
+	{
+		count = sprintf(buf,"HTTP/1.1 404 CANNOT FIND FILE\r\n");
+	}
+	send(fd,buf,count,0);
+
+	count = sprintf(buf,"content-type: text/html\r\n");
+	send(fd,buf,count,0);
+
+	count = sprintf(buf,"\r\n");
+	send(fd,buf,count,0);
 }
 
 
